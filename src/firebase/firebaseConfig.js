0f3002d95a9a6b48edc9a8f46416b76064a60e6d
EnricoPaparo <enrico.paparo@gmail.com>
2026-05-20 19:31:22 +0200

    Add Firebase configuration

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtJ5nVvPsSx8sPRrlSaDUO67sddceUn2Y",
+  authDomain: "gs-giornatesportive.firebaseapp.com",
+  projectId: "gs-giornatesportive",
+  storageBucket: "gs-giornatesportive.firebasestorage.app",
+  messagingSenderId: "1015376487348",
+  appId: "1:1015376487348:web:facdb3cbc027e27a4c08e1"
+};
