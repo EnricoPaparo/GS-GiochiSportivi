3a579117ddff437fed96c87b07daaa62cf4f0c8e
EnricoPaparo <enrico.paparo@gmail.com>
2026-05-20 23:25:00 +0200

    Persist db changes to Firestore

--- src/data/repository.js
+++ src/data/repository.js
@@ -1,8 +1,19 @@
-export {
+import {
   clearSession,
   getDb,
   getSession,
   resetDb,
-  saveDb,
+  saveDb as saveLocalDb,
   saveSession
 } from "./localStorageRepository.js";
+import { saveRemoteDb } from "./firestoreRepository.js";
+
+export { clearSession, getDb, getSession, resetDb, saveSession };
+
+export function saveDb(db) {
+  saveLocalDb(db);
+
+  saveRemoteDb(db).catch((error) => {
+    console.warn("Salvataggio Firestore non riuscito; dati mantenuti nella cache locale.", error);
+  });
+}
